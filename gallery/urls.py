881e12d936b63329b5cf214efef0a2521b418328
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-11-25 23:38:19 +0300

    add urlconfig that intergrate to different page

--- gallery/urls.py
+++ gallery/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url,include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^app/',include('app.urls'))
 ]
