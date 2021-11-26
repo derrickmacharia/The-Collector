a7f517682154cc84ca52af22f7e10c3fdb35f3f9
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-11-26 22:00:41 +0300

    create database

--- gallery/settings.py
+++ gallery/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app.apps.AppConfig',
+    'app',
     'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER' :  'moringa',
+        'PASSWORD' : '12345',
     }
 }
 
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
