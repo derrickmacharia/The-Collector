e5031688e47cb4dc8812a6e29e677a377d7e05b1
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-11-29 08:43:28 +0300

    test category class

---
+++ README.md
@@ -0,0 +1,10 @@
+# Gallery!!
+
+### 29th Nov. 2021
+
+## Author
+
+[Derrick Macharia](https://github.com/derrickmacharia)
+
+# Description
+This is a Python Application where a user(s) can view  a photo, search through category and view according to the location taken.
--- app/tests.py
+++ app/tests.py
@@ -8,10 +8,36 @@ class PhotosTestClass(TestCase):
         Create a image for testing
         """
         photos.objects.create(
-            title="Test Image",
-            description="Test Description",
-            location=Location.objects.create(name="Test Location"),
-            category=Category.objects.create(name="Test Category"),
+            title="Image Test",
+            description="Description Test",
+            location=Location.objects.create(name="Location Test"),
+            category=Category.objects.create(name="Category Test"),
             image="http://test.com/test.jpg",
             created_at=None
         )
+
+
+# category models test
+class CategoryTestCase(TestCase):
+
+    def setUp(self):
+        """
+        Create a category for testing
+        """
+        Category.objects.create(name="Photo Category")
+        
+    def test_category_name(self):
+        """
+        Test that the category name is correct
+        """
+        category = Category.objects.get(name="Photo Category")
+        self.assertEqual(category.name, "Photo Category")
+
+    def test_category_str(self):
+        """
+        Test that the category string representation is correct
+        """
+        category = Category.objects.get(name="Photo Category")
+        self.assertEqual(str(category), "Photo Category")
+
+
